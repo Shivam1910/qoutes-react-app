5fab7dc225a4170a16133b6013fc75a814112213
shivam <shivam04.shrivastava@gmail.com>
2023-02-20 16:15:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+// import axios from "axios";
+import { quotesData } from "./quotes.data";
+
+// advice api url = https://api.adviceslip.com/advice
+// famous quotes api url = https://type.fit/api/quotes
+
+const App = () => {
+  //   const [advice, setAdvice] = useState("Click the button to get an advice");
+  const [quote, setQuote] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [charLength, setCharLength] = useState(0);
+  //   const [disable, setDisable] = useState(false);
+
+  //   const fetchAdvice = async () => {
+  //     setLoading(true);
+  //     const response = await axios.get(`https://api.adviceslip.com/advice`);
+  //     const { advice } = response.data.slip;
+  //     setLoading(false);
+
+  //     setAdvice(advice);
+  //     fetchQuotes();
+  //   };
+  const fetchQuotes = () => {
+    // const response = await axios.get("https://type.fit/api/quotes");
+    // console.log(response.data);
+    setLoading(true);
+    const randomQuote =
+      quotesData[Math.floor(Math.random() * quotesData.length)];
+    setCharLength(randomQuote.text.length);
+    setLoading(false);
+    setQuote({ quote: randomQuote.text, author: randomQuote.author });
+  };
+
+  useEffect(() => {
+    //   fetchAdvice();
+    fetchQuotes();
+  }, []);
+
+  if (!loading) {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className={charLength > 80 ? "heading-small" : "heading"}>
+            {quote.quote}
+          </h1>
+          <p className="author">-{quote.author ? quote.author : "anonymous"}</p>
+          <button
+            className="button"
+            onClick={fetchQuotes}
+            disabled={loading && true}
+          >
+            Generate Quote
+          </button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">Loading quote...</h1>
+        <button
+          className="button"
+          onClick={fetchQuotes}
+          disabled={loading && true}
+        >
+          Generate Quote
+        </button>
+      </div>
+    </div>
+  );
+};
+export default App;
